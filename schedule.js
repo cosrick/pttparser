7b7515900c6a3c0b3a2c59fc7f871b26285005db
李昱賢 <rick830620@gmail.com>
2016-07-14 00:40:24 +0800

    adjust schedule to every 2

--- schedule.js
+++ schedule.js
@@ -3,4 +3,4 @@ var CronJob = require('cron').CronJob;
 var timeZone= "Asia/Singapore";
 var ptt 	= require('./pttParser.js');
 
-new CronJob('0 */3  * * * *', function () { ptt() }, null, true, timeZone); 
+new CronJob('0 */2  * * * *', function () { ptt() }, null, true, timeZone); 

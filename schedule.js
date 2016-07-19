70fde9c1098457bdb2ba3bb5ac256fc19e086e59
李昱賢 <rick830620@gmail.com>
2016-07-19 15:59:31 +0800

    change frequence to 6min

--- schedule.js
+++ schedule.js
@@ -3,4 +3,4 @@ var CronJob = require('cron').CronJob;
 var timeZone= "Asia/Singapore";
 var ptt 	= require('./pttParser.js');
 
-new CronJob('0 */2  * * * *', function () { ptt() }, null, true, timeZone); 
+new CronJob('0 4,10,16,22,28,34,40,46,52,58  * * * *', function () { ptt() }, null, true, timeZone); 
